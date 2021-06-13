5c5688c76891bb4e6ee95198e04e2372c0700271
Mika C <59329994+Avnsx@users.noreply.github.com>
2021-06-13 02:08:51 +0200

    Update source.py
    added catch exception NameError, for user clarity

--- source.py
+++ source.py
@@ -7,9 +7,9 @@ userlist=re.sub('\r\n', '\n', str(requests.get('http://pastebin.com/raw/VtUHCwE6
 for x in userlist:ua_list.append(x)
 random.shuffle(ua_list)
 def get_useragent():return(str(random.choice(ua_list)))
-pers_UA=get_useragent()
-headers={'user-agent': pers_UA,'accept-language': 'en-US,en;q=0.9',}
+headers={'user-agent': get_useragent(),'accept-language': 'en-US,en;q=0.9',}
 
+####################################################################################
 
 def refresh_opgg():
     opgg_update = requests.post('https://'+Region+'.op.gg/summoner/ajax/renew.json/', headers=headers, data={'summonerId': re.findall(r'Id=[0-9]+', str(opgg_website.content))[0].strip('Id=')})
@@ -17,7 +17,7 @@ def refresh_opgg():
     elif opgg_update.status_code == 418 or 504:print('Got rate limited, trying to update again in a few seconds; relaying on older stats now.')
     else:
         print('\n'+opgg_update.text)
-        print('\nERROR: Op.gg responded unexpectedly (Code: '+opgg_update.status_code+'); Copy & Paste this error into Github > Issues.\n')
+        print('ERROR: Op.gg responded unexpectedly (Code: '+opgg_update.status_code+'); Copy & Paste this error into Github > Issues.\n')
         s(10)
 
 def get_stats():
@@ -33,7 +33,7 @@ def get_stats():
     for x in range(0, 10):
         game_history.append(r.html.find('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.RealContent > div > div.Content > div.GameItemList > div:nth-child('+str(x_val)+') > div > div.Content > div.GameStats > div.TimeStamp > span', first=True).text+':'+r.html.find('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.RealContent > div > div.Content > div.GameItemList > div:nth-child('+str(x_val)+') > div > div.Content > div.GameStats > div.GameResult', first=True).text)
         x_val+=1
-    blacklist = ['10 hours','11 hours','12 hours','13 hours','14 hours','15 hours','16 hours','17 hours','18 hours','19 hours','20 hours','21 hours','22 hours','23 hours','day']
+    blacklist = ['day','9 hours','10 hours','11 hours','12 hours','13 hours','14 hours','15 hours','16 hours','17 hours','18 hours','19 hours','20 hours','21 hours','22 hours','23 hours']
     filtered_stats = [f for f in game_history if all([word not in f for word in blacklist])]
 
     matches=[]
@@ -52,7 +52,7 @@ def get_stats():
         except:wins = 0
         try:lose = str(counted_today_dict['Defeat'])
         except:lose = 0
-        resp=str('Games: '+str(len(stats_today)/2).split('.')[0]+' | '+'W '+str(wins)+' - '+'L '+str(lose))
+        resp=str('Games: '+str(len(stats_today)/2).split('.')[0]+' | '+'W'+str(wins)+' - '+'L'+str(lose))
         print(resp)
         return resp
     except Exception as e:
@@ -66,7 +66,11 @@ if __name__ == '__main__':
     while True:
         try:opgg_updt=get_stats()
         except:pass
-        with open('stats.txt', 'w', encoding="utf-8") as output:output.write(opgg_updt)
+        try:
+            with open('stats.txt', 'w', encoding="utf-8") as output:output.write(opgg_updt)
+        except NameError:
+            print('Empty response from opgg; looks like your username in line 63 is wrong')
+            exit()
         r.session.close()
         r.close()
         s(60) # time to wait until next update in seconds
